987407c4471f4b3a521fb4c33faafd8307810d72
Michael Howell <michael@notriddle.com>
2019-09-10 15:38:24 -0700

    Add redirect trap

--- web/index.php
+++ web/index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang=en>
-	<head>
-		<title>My Awesome Page</title>
-		<meta name=viewport content="width=device-width, initial-scale=1.0">
-		<meta name=charset content=utf-8>
-		<meta name=robots content="noindex, nofollow">
-	</head>
-	<body>
-		<?php
+<?php
 /*
  * BotPoison - Bad bot poisoning system
  * As the name implies, BotPoison "poisons" bad Web crawlers.
@@ -24,6 +15,25 @@
  			$c = file_get_contents($counter);
 			$p = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 			$l = strlen($p) - 1;
+			if (isset($_GET['id']) && $_GET['id'] % 2 === 0) {
+				header('Location: '.$link . "?id=" . $_GET['id']);
+				exit();
+			}
+			if (isset($_GET['q'])) {
+				header('Location: '.$link . "?q=" . $p[rand(0, $l - 1)]);
+				exit();
+			}
+
+?><!DOCTYPE html>
+<html lang=en>
+	<head>
+		<title>My Awesome Page</title>
+		<meta name=viewport content="width=device-width, initial-scale=1.0">
+		<meta name=charset content=utf-8>
+		<meta name=robots content="noindex, nofollow">
+	</head>
+	<body>
+<?php
  			for ($i=0; $i < 75000; $i++) {
  				print '<a href="' . $link . "?id=" . ($i + $c) . '">';
 				for ($j = 0; $j < rand(1, 10); ++$j) {
